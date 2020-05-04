1bef3d6ec0d998477a5f787a04cfb976efd8f8bb
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-05-04 12:02:06 -0400

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,40 @@
-{
-let shoot = Math.random();      
-let shots = Math.floor(Math.random())+1;
+const reset_button = document.querySelector("#reset_button")
+reset_button.addEventListener("click", function() {
+})
+
+let goal = Math.floor(Math.random()) + 1;
+let shots = Math.floor(Math.random()) + 1;
 const teamone_numshots = document.querySelector("#teamone_numshots");
 const teamone_numgoals = document.querySelector("#teamone_numgoals");
 const teamone_shoot_button = document.querySelector("#teamone_shoot_button");
-teamone_shoot_button.addEventListener("click", function () {
+teamone_shoot_button.addEventListener("click", function() {
+    let shoot = Math.random();
     teamone_numshots.innerText = shots;
     shots = shots + 1
     console.log(shoot);
     if (shoot > 0.5) {
-        alert("success");
+      console.log("success");
+      teamone_numgoals.innerText = goal;
     }
     else {
-        alert("fail");
+      console.log("fail");
     }
 })
-}
-{
-let shoot1 = Math.random();      
-let shots1 = Math.floor(Math.random())+1;
+let shots1 = Math.floor(Math.random()) + 1;
+let goal1 = Math.floor(Math.random()) + 1;
 const teamtwo_numshots = document.querySelector("#teamtwo_numshots");
 const teamtwo_numgoals = document.querySelector("#teamtwo_numgoals");
 const teamtwo_shoot_button = document.querySelector("#teamtwo_shoot_button");
-teamtwo_shoot_button.addEventListener("click", function () {
-    console.log(shoot1);
-   teamtwo_numshots.innerText = shots1;
+teamtwo_shoot_button.addEventListener("click", function() {
+    let shoot1= Math.random();
+    teamtwo_numshots.innerText = shots1;
     shots1 = shots1 + 1
+    console.log(shoot1);
     if (shoot1 > 0.5) {
-        alert("success");
+        teamtwo_numgoals.innerText = goal1;
+        console.log("success");
     }
     else {
-        alert("fail");
+      console.log("fail");
     }
 })
-}
