6a2d8ac8573655358f04cb8ee2110166785f415a
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-05-04 12:53:39 -0400

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,8 @@
 const reset_button = document.querySelector("#reset_button")
 reset_button.addEventListener("click", function() {
 })
-
-let goal = Math.floor(Math.random()) + 1;
-let shots = Math.floor(Math.random()) + 1;
+let goal = 0;
+let shots = 1;
 const teamone_numshots = document.querySelector("#teamone_numshots");
 const teamone_numgoals = document.querySelector("#teamone_numgoals");
 const teamone_shoot_button = document.querySelector("#teamone_shoot_button");
@@ -14,14 +13,15 @@ teamone_shoot_button.addEventListener("click", function() {
     console.log(shoot);
     if (shoot > 0.5) {
       console.log("success");
+      goal = goal + 1
       teamone_numgoals.innerText = goal;
     }
     else {
       console.log("fail");
     }
 })
-let shots1 = Math.floor(Math.random()) + 1;
-let goal1 = Math.floor(Math.random()) + 1;
+let shots1 = 1;
+let goal1 = 0;
 const teamtwo_numshots = document.querySelector("#teamtwo_numshots");
 const teamtwo_numgoals = document.querySelector("#teamtwo_numgoals");
 const teamtwo_shoot_button = document.querySelector("#teamtwo_shoot_button");
@@ -31,8 +31,9 @@ teamtwo_shoot_button.addEventListener("click", function() {
     shots1 = shots1 + 1
     console.log(shoot1);
     if (shoot1 > 0.5) {
-        teamtwo_numgoals.innerText = goal1;
         console.log("success");
+        goal1 = goal1 + 1
+        teamtwo_numgoals.innerText = goal1;
     }
     else {
       console.log("fail");
